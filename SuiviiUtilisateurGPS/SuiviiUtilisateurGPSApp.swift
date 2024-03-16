6ad1a3ddf55195507b36f4fd83bfcded3ec71bd1
Philippe MICHEL <philippe.michel1@gmail.com>
2024-03-16 18:08:47 +0100

    Initial Commit

---
+++ SuiviiUtilisateurGPS/SuiviiUtilisateurGPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuiviiUtilisateurGPSApp.swift
+//  SuiviiUtilisateurGPS
+//
+//  Created by Philippe MICHEL on 16/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SuiviiUtilisateurGPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
